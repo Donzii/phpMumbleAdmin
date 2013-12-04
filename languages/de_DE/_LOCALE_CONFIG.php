8c9d56f52e34b0aa2e69672ac671fbf3842f1d60
Donzi <Donzi@Despe.de>
2013-12-04 18:11:02 +0100

    Uebersetzung noch nicht vollstaendig

--- languages/de_DE/_LOCALE_CONFIG.php
+++ languages/de_DE/_LOCALE_CONFIG.php
@@ -2,15 +2,7 @@
 
 /*
 
-Set language name
-
-EXAMPLE:
-
-$localeConf = array( 'name' => 'French', 'localized' => 'Français' );
-
-'name' => 'French' : set the english name of the language ( it's not mandatory )
-
-'localized' => 'Français' : set the localized name ( it's not mandatory )
+Übersetzung noch nicht vollstaendig!
 
 */
 
