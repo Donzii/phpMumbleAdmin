a2e03591e53d5202f584bfdd8cc6d38b6b86e342
Donzi <Donzi@Despe.de>
2014-01-14 10:05:20 +0100

    Removed Querytracker, Updated sh
    GTMurmur can only scan 1 Server-Instance.
    If you start a 2nd Serverinstance, GTmurmur gives no response.

--- murmur_server/murmur.sh
+++ murmur_server/murmur.sh
@@ -12,17 +12,10 @@
 #                                                         #
 ###########################################################
 
-###########################################################
-# 			GT MURMUR PLUGIN                              #
-# http://www.gametracker.com/downloads/gtmurmurplugin.php #
-#                                                         #
-###########################################################
 
 #	Name der für das Script ausgeben wird
 NAME="Mumble Server"
 
-#	Name der für das Script ausgeben wird
-NAME2="GameTracker-Query"
 
 #	Userverzeichnis für Murmur
 USERDIR="/home/mumble"
@@ -30,15 +23,8 @@ USERDIR="/home/mumble"
 #	Arbeitsverzeichnis für Murmur
 BASEDIR="/home/mumble/server"
 
-#	Arbeitsverzeichnis für GameTracker-Query Murmur
-BASEDIR2="/home/mumble/querytracker"
-
 #	Das Startcommando für Murmur
 COMMAND="murmur_server.x86"
-
-#	Das Startcommando für  GameTracker-Query Murmur
-COMMAND2="gtmurmur-static"
-
 
 #	Die ini-Daten von murmur
 INI=murmur.ini
@@ -46,8 +32,6 @@ INI=murmur.ini
 #	Hier bewahrt das Script den Murmur-PID auf um später den Server wieder stoppen zu können
 PIDFILE=murmur.pid
 
-#	Hier bewahrt das Script den GameTracker-Query-PID auf um später den Server wieder stoppen zu können
-PIDFILE2=gtmurmur-static.pid
 
 #	Updateverzeichnis für Murmur
 UPDATEDIR="/home/mumble/update"
@@ -91,6 +75,7 @@ case "$1" in
 
 ;;
 
+
 	stop)
 		cd $BASEDIR
 		if test -f $PIDFILE; then
@@ -115,6 +100,7 @@ case "$1" in
 	fi
 ;;
 
+
 	restart)
 		cd $BASEDIR
 		PID="`cat $PIDFILE`"
@@ -126,14 +112,6 @@ case "$1" in
    		rm -f $PIDFILE
 		touch $PIDFILE
 		chmod 750 $COMMAND
-		cd $BASEDIR2
-		PID="`cat $PIDFILE2`"
-		echo ""
-		echo "  |  > $NAME2 '$PID' wird gestoppt ..."
-		echo ""
-		kill $PID
-		killall -9 $COMMAND2
-		rm -f $PIDFILE2
 		sleep 5
 		cd $BASEDIR
 		./$COMMAND -ini $INI
@@ -141,63 +119,8 @@ case "$1" in
 		echo ""
 		echo "  |  > $NAME erfolgreich wieder hergestellt. Prozess-ID '$PID'"
 		echo ""
-		cd $BASEDIR2
-		nice -n 19 ./$COMMAND2 -conf $BASEDIR/$INI 2> /dev/null >&2 &
-		ps -ef | grep $COMMAND2 | grep -v grep | awk ' { print $2 }' > $PIDFILE2
-		PID="`cat $PIDFILE2`"
-		echo ""
-		echo "  |  > $NAME2 erfolgreich wieder hergestellt. Prozess-ID '$PID'"
-		echo ""
 ;;
 
-
-	querystart)
-		cd $BASEDIR2
-		if test -f $PIDFILE2; then
-			PID="`cat $PIDFILE2`"
-   			echo ""
-			echo "  |  > PID-File vorhanden. Prozess-ID '$PID' . Server abgestürzt?"
-			echo ""
-		elif test $PIDFILE2; then
-			echo ""
-			echo "  |  > Prozess-ID und PID-File nicht vorhanden, starte den $NAME2"
-			echo ""
-			
-			touch $PIDFILE2
-			chmod 750 $COMMAND2
-			nice -n 19 ./$COMMAND2 -conf $BASEDIR/$INI 2> /dev/null >&2 &
-			ps -ef | grep $COMMAND2 | grep -v grep | awk ' { print $2 }' > $PIDFILE2
-			PID="`cat $PIDFILE2`"
-			echo ""
-			echo "  |  > $NAME2 erfolgreich wieder hergestellt. Prozess-ID '$PID'"
-			echo ""
-	fi
-
-;;
-
-	querystop)
-		cd $BASEDIR2
-		if test -f $PIDFILE2; then
-			echo ""
-			echo "  |  > Stoppe den $NAME2"
-			echo ""
-			PID="`cat $PIDFILE2`"
-			kill $PID
-			killall -9 $COMMAND2
-			echo ""
-			echo "  |  > $NAME2 wurde beendet"
-			echo ""
-			rm -f $PIDFILE2
-			echo ""
-			echo "  |  > $PIDFILE2 wurde entfernt"
-			echo ""
-			chmod 644 $COMMAND2
-		elif test $PIDFILE2; then
-			echo ""
-			echo "  |  > Der $NAME2 läuft nicht."
-			echo ""
-	fi
-;;
 
 	update)
 		cd $BASEDIR
@@ -252,6 +175,7 @@ case "$1" in
 		echo ""
 ;;
 
+
 	fullbackup)
 		cd $BASEDIR
 		PID="`cat $PIDFILE`"
@@ -262,14 +186,7 @@ case "$1" in
 		killall -9 $COMMAND
    		rm -f $PIDFILE
 		touch $PIDFILE
-		chmod 750 $COMMAND
-		cd $BASEDIR2
-		PID="`cat $PIDFILE2`"
-		echo ""
-		echo "  |  > $NAME2 '$PID' wird gestoppt ..."
-		echo ""
-		kill $PID
-		rm -f $PIDFILE2
+		chmod 644 $COMMAND
 		sleep 2
 		echo ""
 		echo "  |  > FullBackup wird erstellt ..."
@@ -278,23 +195,16 @@ case "$1" in
 		./backup.sh
 		sleep 2
 		cd $BASEDIR
+		chmod 750 $COMMAND
 		./$COMMAND -ini $INI
 		PID="`cat $PIDFILE`"
 		echo ""
 		echo "  |  > $NAME erfolgreich wieder hergestellt. Prozess-ID '$PID'"
 		echo ""
-		cd $BASEDIR2
-		nice -n 19 ./$COMMAND2 -conf $BASEDIR/$INI 2> /dev/null >&2 &
-		ps -ef | grep $COMMAND2 | grep -v grep | awk ' { print $2 }' > $PIDFILE2
-		PID="`cat $PIDFILE2`"
-		echo ""
-		echo "  |  > $NAME2 erfolgreich wieder hergestellt. Prozess-ID '$PID'"
-		echo ""
-		
 ;;
 
 *)
-	echo "Usage: $0 Parameter eingeben {start|stop|restart|querystart|querystop|update|fullbackup}"
+	echo "Usage: $0 Parameter eingeben {start|stop|restart|update|fullbackup}"
 	exit 1
 ;;
 esac
